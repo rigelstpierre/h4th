5209b57637788aa7bb38f4e1cb4b2f5ce43f48b3
Rigel St. Pierre <rigel@rigel.co>
2017-05-21 08:09:17 -0400

    ready for launch

--- src/Stories.js
+++ src/Stories.js
@@ -10,10 +10,30 @@ class Stories extends Component {
 
         </div>
         <div className="story-container" id="joshandamy">
-
+          <div className="story">
+            <p>
+              We love our church and see H4TH as an investment into it's future and our family. We want to see church moving forward, taking ground and reaching people in this city.
+              <br />
+              <br />
+              We give because we've seen time and time again how God is faithful. When we put His House first and build that, He is always faithful to take care of us. We give because we want our lives and finances to be attached to something eternal.   
+              <br />
+              <br />
+              <strong>Josh & Amy </strong>
+            </p>
+          </div>
         </div>
         <div className="story-container" id="errolandbrenda">
-
+          <div className="story">
+            <p>
+              Heart for the House is about hearts being changed, souls being saved and lives being impacted.  It's about generosity in all areas of life. Its about putting a stake in the ground and supporting our pastors, and our church family.  It's about being a sower and valuable contributor to our mission as a church.  It is about what God wants to do in the lives of people. It is about experiencing the amazing outpouring of grace and love that only Jesus can give, putting everything in His
+              <br />
+              <br />
+              hands and trusting in Him.  It is about being the hands and feet of Jesus and it's about doing everything with LOVE. We can't wait to see what God is going to do with the generosity of his people through our Heart for the House this year! It just keeps getting better and better!
+              <br />
+              <br />
+              <strong>Errol and Brenda</strong>
+            </p>
+          </div>
         </div>
         <div className="story-container" id="jorgeandlorraine">
 
